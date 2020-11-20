480bac743f6e7946a4e84c1e98cd0a8b204dea23
codingandcaffeine <codingandcaffeine@outlook.com>
2020-11-20 17:11:16 +0100

    refresh setup project for MVVM - part 1

---
+++ TODODesktopUI/ViewsModels/MyTodosViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODODesktopUI.ViewsModels
+{
+    class MyTodosViewModel
+    {
+
+        private string _newTodo = "Hello World";
+
+        public string NewTodo
+        {
+            get { return _newTodo; }
+            set { _newTodo = value; }
+        }
+
+    }
+}
