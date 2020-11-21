59f14fef99247a4db54cf7deb6d93a61b79ddd89
codingandcaffeine <codingandcaffeine@outlook.com>
2020-11-21 15:35:07 +0100

    added observable collection - readdy to install mvvm light

--- TODODesktopUI/ViewsModels/MyTodosViewModel.cs
+++ TODODesktopUI/ViewsModels/MyTodosViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TODODesktopUI.Library;
 
 namespace TODODesktopUI.ViewsModels
 {
@@ -10,12 +12,37 @@ namespace TODODesktopUI.ViewsModels
     {
 
         private string _newTodo = "Hello World";
-
         public string NewTodo
         {
             get { return _newTodo; }
-            set { _newTodo = value; }
+            set { 
+                _newTodo = value;
+            
+            }
         }
 
+        private ObservableCollection<Todo> _todolist;
+        public ObservableCollection<Todo> Todolist
+        {
+            get { return _todolist; }
+            set { _todolist = value; }
+        }
+
+
+        public MyTodosViewModel()
+        {
+            // Initialize instance of todolist
+            Todolist = new ObservableCollection<Todo>();
+
+            // Load demo data
+            Todolist.Add(new Todo() { Title = "Todo 1", IsCompleted=true});
+            Todolist.Add(new Todo() { Title = "Todo 2", IsCompleted=false});
+            Todolist.Add(new Todo() { Title = "Todo 3", IsCompleted=true});
+        }
+
+        public void AddTodo()
+        {
+
+        }
     }
 }
